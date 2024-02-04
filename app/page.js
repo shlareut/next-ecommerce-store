a8d253fbfa741aeab8e13c39ecd67f2b581407c6
Lars Reuter <lp.reuter@gmail.com>
2024-02-04 17:35:01 +0100

    Add DB products and render in listing

--- app/page.js
+++ app/page.js
@@ -64,12 +64,58 @@ export default async function HomePage() {
         })}
       </div>
       {/* DB Products start below */}
-      <div>
-        <ul>
-          {dbProducts.map((item) => {
-            return <li key={item.id}>{item.title}</li>;
-          })}
-        </ul>
+      <h1 className={styles.productListingsTitle}>Database junk</h1>
+      <div className={styles.productListing}>
+        {dbProducts.map((product) => {
+          const strikePrice = Math.round(product.price * 1.5);
+          return product.ispublished ? (
+            <div className={styles.productCard} key={`product-${product.id}`}>
+              <Link
+                href={`/productDetails/${product.id}`}
+                // target="_blank"
+                // rel="noopener noreferrer"
+              >
+                {product.isDeal ? (
+                  <div className={styles.deal}>🔥 Hot junk</div>
+                ) : (
+                  ''
+                )}
+                <Image
+                  className={styles.productImage}
+                  alt="product"
+                  src={product.image}
+                  width={262}
+                  height={262}
+                />
+                <div className={styles.productText}>
+                  <p className={styles.productTitle}>{product.title}</p>
+                  <p className={styles.productProperties}>
+                    <span className={styles.productCondition}>
+                      {product.condition}
+                    </span>{' '}
+                    {product.category}
+                  </p>
+                </div>
+                <div className={styles.productPrice}>
+                  <p>
+                    {product.isdeal ? (
+                      <span className={styles.productStrikePrice}>
+                        {strikePrice}
+                        {product.currency}
+                      </span>
+                    ) : (
+                      ''
+                    )}{' '}
+                    {Math.round(product.price)}
+                    {product.currency}
+                  </p>
+                </div>
+              </Link>
+            </div>
+          ) : (
+            ''
+          );
+        })}
       </div>
     </>
   );
