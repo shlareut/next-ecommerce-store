ddc15c254383a0857b11855245fd685257ad7b42
Lars Reuter <lp.reuter@gmail.com>
2024-02-15 15:22:06 +0100

    Add loading placeholder image

--- app/productDetails/[productId]/ImageComponent.js
+++ app/productDetails/[productId]/ImageComponent.js
@@ -2,32 +2,22 @@
 import React, { useEffect, useState } from 'react';
 
 export default function ImageComponent(props) {
-  const [isLoading, setIsLoading] = useState(true);
   const [imageUrl, setImageUrl] = useState('/images/loading.jpeg');
   useEffect(() => {
-    const setImage = async () => {
+    const setImage = () => {
       const newImage = props.image;
       setImageUrl(newImage);
-      setIsLoading(false);
     };
-    setImage().catch((error) => {
-      console.log(error);
-    });
+    setImage();
     console.log('Fetch!');
   }, [props.image]);
   return (
-    <div>
-      {isLoading ? (
-        'Loading...'
-      ) : (
-        <img
-          data-test-id="product-image"
-          alt="test123"
-          src={imageUrl}
-          width={100}
-          height={100}
-        />
-      )}
-    </div>
+    <img
+      data-test-id="product-image"
+      alt="test123"
+      src={imageUrl}
+      width={100}
+      height={100}
+    />
   );
 }
